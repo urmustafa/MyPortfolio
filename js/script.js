44803e0fe21774d02f8a8787fac374ae901900e9
Mustafa Tayyar <mtayyar77@gmail.com>
2023-08-12 01:07:16 +0300

    my portfolio

---
+++ js/script.js
@@ -0,0 +1,54 @@
+
+let sections=document.querySelectorAll('section')
+let navLinks=document.querySelectorAll('header nav  a')
+
+let menuIcon=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+    console.log("tık");
+}
+
+
+
+window.onscroll=()=>{
+
+    sections.forEach(sec=>{
+        console.log("object");
+        let top=window.scrollY;
+        let offset =sec.offsetTop -100;
+        let height=sec.offsetHeight;
+        console.log(height);
+        let id =sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height ){
+            console.log("ifiniçindeyim");
+            navLinks.forEach(links=>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*='+ id +']').classList.add('active')
+            })
+
+            sec.classList.add('show-animate');
+        }
+        else{
+            sec.classList.remove('show-animate')
+        }
+
+    });
+
+
+
+    let header=document.querySelector('header');
+
+    header.classList.toggle('sticky',window.scrollY>100);
+
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+    let footer=document.querySelector('footer');
+    footer.classList.toggle('show-animate',this.innerHeight+this.scrollY>=document.scrollingElement.scrollHeight)
+
+}
